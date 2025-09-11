4381461cda1434afe1521392e611fabfa0b55d74
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-09-11 09:12:25 -0500

    ElementIdGenerator count

--- src/element_id_generator.ts
+++ src/element_id_generator.ts
@@ -24,20 +24,30 @@ export class ElementIdGenerator {
    * { bunchId: beforeId.bunchId, counter: beforeId.counter + 1 }
    * ```
    * which compresses well when inserted after beforeId.
+   *
+   * @param count Provide this if you are planning to bulk-insert `count` ids from left-to-right
+   * (by supplying the same `count` to insertAfter/insertBefore).
+   * Only the first id is returned, but we will reserve space for the other ids so that
+   * they are not generated again.
+   * @throws If count is provided but is not >= 1.
    */
-  generateAfter(beforeId: ElementId | null): ElementId {
+  generateAfter(beforeId: ElementId | null, count = 1): ElementId {
+    if (!(Number.isSafeInteger(count) && count >= 1)) {
+      throw new Error(`Invalid count: ${count}`);
+    }
+
     if (beforeId !== null) {
       const nextCounter = this.nextCounterMap.get(beforeId.bunchId);
       if (nextCounter !== undefined && nextCounter === beforeId.counter + 1) {
         // It's our own ElementId and the last in its bunch. Extend the bunch.
-        this.nextCounterMap.set(beforeId.bunchId, nextCounter + 1);
+        this.nextCounterMap.set(beforeId.bunchId, nextCounter + count);
         return { bunchId: beforeId.bunchId, counter: nextCounter };
       }
     }
 
     // In all other cases, start a new bunch.
     const bunchId = this.newBunchId();
-    this.nextCounterMap.set(bunchId, 1);
+    this.nextCounterMap.set(bunchId, count);
     return { bunchId, counter: 0 };
   }
 }
