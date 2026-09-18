ed19ff804389597bf010c64e6f02c11b93a72341
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2026-09-17 21:53:17 -0500

    document SavedIdList relaxation

--- src/saved_id_list.ts
+++ src/saved_id_list.ts
@@ -4,6 +4,10 @@
  * It describes all of the list's known ElementIds in list order, with basic compression:
  * if sequential ElementIds have the same bunchId, the same isDeleted status,
  * and sequential counters, then they are combined into a single object.
+ *
+ * You may create or modify SavedIdLists manually. Duplicate ElementIds
+ * are not allowed, but it is okay to have objects with `count == 0`
+ * or neighbors that are not fully combined.
  */
 export type SavedIdList = Array<{
   readonly bunchId: string;
